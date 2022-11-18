f63352af253709d945960115dc6511acac33b26d
Sonja Lechner <blaquekaktus@gmail.com>
2022-11-18 01:54:08 +0100

    Daten ins Datenbank hinzufügen!
    Durch PreparedStatement werden neue Student:innen hinzugefügt

--- src/main/java/com/ITKolleg/JDBC_Demo.java
+++ src/main/java/com/ITKolleg/JDBC_Demo.java
@@ -9,17 +9,47 @@ public class JDBC_Demo {
         System.out.println("JDBC Demo!\n");
         //INSERT INTO `student` (`id`, `name`, `email`) VALUES (NULL, 'Jason Lechner', 'jaslechner@tsn.at'), (NULL, 'Josan Lechner', 'joslechner@tsn.at');
         //CREATE TABLE `studenten`.`student` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(200) NOT NULL , `email` VARCHAR(200) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-   selectAllDemo();
+        selectAllDemo();
+        insertStudentDemo();
+        selectAllDemo();
+
+    }
+
+    public static void  insertStudentDemo(){
+        System.out.println("\nInsert Demo mit JDBC\n");
+        String connectionURL = "jdbc:mysql://localhost:3306/jdbc_demo"; //
+        String user = "root";
+        String pwd = "";
+
+        try (Connection conn = DriverManager.getConnection(connectionURL, user, pwd)) {
+            System.out.println("\nVerbinding zur Datenbank hergestellt!\n");
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    //"INSERT  INTO `student` (`id`, `name`, `email`) VALUES (NULL,`"Susan-Jolie Lechner"`,`"sulechner@gmx.at"`)"
+                    // SQL Insert statements sollen so nicht gemacht um SQL Injections zu vermeiden.
+
+                    "INSERT  INTO `student` (`id`, `name`, `email`) VALUES (NULL, ?, ?)" // ? is a placeholder to be replaced later with actual values
+            );
+            try {
+                preparedStatement.setString(1,"Susan-Jolie Lechner");     //sets the value for the name in der vorherige SQL INSERT statement.
+                preparedStatement.setString(2,"sulechner@gmx.at"); //sets the value for the email in der vorherige SQL INSERT statement.
+                int rowsAffected = preparedStatement.executeUpdate();
+                System.out.println(rowsAffected + " Datensätze eingefügt.");
+            }catch(SQLException ex){
+                System.out.println("Folgende Fehler SQL INSERT Statement gefunden: " + ex.getMessage());
+            }
+        } catch (SQLException e) {
+            System.out.println("Folgende Fehler beim Verbingsversuch ist aufgetreten: " + e.getMessage());
+        }
     }
 
     public static void selectAllDemo() {
-        System.out.println("Select Demo mit JDBC\n");
+        System.out.println("\nSelect Demo mit JDBC\n");
         String sqlSelectAllPersons = "Select * FROM `student`";
         String connectionURL = "jdbc:mysql://localhost:3306/jdbc_demo"; //
 
 
         try (Connection conn = DriverManager.getConnection(connectionURL, "root", "")) {
-            System.out.println("Verbinding zur Datenbank hergestellt!\n");
+            System.out.println("\nVerbinding zur Datenbank hergestellt!\n");
             PreparedStatement preparedStatement = conn.prepareStatement("Select * FROM `student`");
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()){
