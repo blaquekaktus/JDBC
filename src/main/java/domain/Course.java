0f96f12f13cef4d63e0ae4c97e98315fd9990cfc
Sonja Lechner <blaquekaktus@gmail.com>
2022-11-19 14:38:53 +0100

    Class Course updated

--- src/main/java/domain/Course.java
+++ src/main/java/domain/Course.java
@@ -64,11 +64,11 @@ public class Course extends IDEntity{
      * @param description is used to set the value of the instance variable description
      * @throws InvalidValueException an error message is displayed in case the value entered does not meet the conditions set by this method
      */
-    public void setDescription(String description) {
-        if((description != null) || (description.length()>1)) {       // sets a condition that the name may not be empty(null) or 1 character or less
+    public void setDescription(String description) throws InvalidValueException{
+        if((description != null) || (description.length()>10)) {       // ensures that the description name is not empty(null) or less than 1 character long
             this.description = description;
-        }else{                                                        // the following error message is displayed if the above condition is not met.
-            throw new InvalidValueException("Kursdescription muss mindestens 2 Zeichen lang sein!");
+        }else{                                                        // displays an error message if the above condition is not met.
+            throw new InvalidValueException("Kursdescription muss mindestens 10 Zeichen lang sein!");
         }
     }
 
@@ -82,15 +82,15 @@ public class Course extends IDEntity{
 
 
     /**
-     * Sets the value of the instance variable name
+     * Sets the value of the instance variable hours
      * @param hours is used to set the value of the instance variable setHours
-     * @throws InvalidValueException an error message is displayed in case the value entered does not meet the conditions set be this method
+     * @throws InvalidValueException an error message is displayed in case the value entered does not meet the conditions set by this method
      */
-    public void setHours(int hours) {
-        if(hours != 0) {                                // sets the condition that the hours may not be 0
+    public void setHours(int hours) throws InvalidValueException{
+        if(hours > 0 && hours < 10) {                   // sets the condition that the hours must be between 1 and 10
             this.hours = hours;
         }else{                                          // the following error message is displayed if the condition is not met
-            throw new InvalidValueException("Kursname muss mindestens 2 Zeichen lang sein!");
+            throw new InvalidValueException("Kurs Stunden darf nur zwischen 1 und 10 seinn!");
         }
     }
 
@@ -107,10 +107,19 @@ public class Course extends IDEntity{
      * @param beginDate is used to set the value of the instance variable beginDate
      * @throws InvalidValueException an error message is displayed in case the value entered for name does not meet the conditions set be this method
      */
-    public void setBeginDate(Date beginDate) {
-        if(beginDate != null) {                         // sets a condition that the date may not be empty(null) or 1 character or less
-            this.beginDate = beginDate;
-        }else{                                          // the following error message is displayed if the above condition is not met.
+    public void setBeginDate(Date beginDate) throws InvalidValueException{
+        if(beginDate != null) {                             //checks that the start date is set
+            if(this.endDate != null) {                      //checks that the end date is already set
+                if (beginDate.before(this.endDate)) {       //checks that the startdate is before the end date
+                    this.beginDate = beginDate;
+                } else {
+                    throw new InvalidValueException("Kursbeginn muss VOR Kursende sein!"); //throws an exception if the start date is not before the end date
+                }
+            }else{
+                this.beginDate = beginDate;
+            }
+        }else{
+            throw new InvalidValueException("Startdatum darf nicht null/leer sein!");
         }
     }
 
@@ -127,11 +136,19 @@ public class Course extends IDEntity{
      * @param endDate is used to assign the value of the instance variable endDate
      * @throws InvalidValueException an error message is displayed in case the value entered for name does not meet the conditions set be this method
      */
-    public void setEndDate(Date endDate) {
-        if(endDate != null){                            // sets a condition that the date may not be empty(null)
-            this.endDate = endDate;
-        }else{                                          // the following error message is displayed if the above condition is not met.
-            throw new InvalidValueException("Kursname muss mindestens 2 Zeichen lang sein!");
+    public void setEndDate(Date endDate) throws InvalidValueException{
+        if(endDate != null) {                              //checks that the end date is set
+            if(this.beginDate != null) {                   //checks that the start date is already set
+                if (endDate.after(this.beginDate)) {       //checks that the end date is after the start date
+                    this.endDate = endDate;
+                } else {
+                    throw new InvalidValueException("Kursende muss NACH Kursbegin sein!"); //throws an exception if the end date is not after the start date
+                }
+            }else{
+                this.beginDate = beginDate;
+            }
+        }else{
+            throw new InvalidValueException("Enddatum darf nicht null/leer sein!");
         }
     }
 
@@ -146,13 +163,28 @@ public class Course extends IDEntity{
     /**
      * Sets the value of the instance variable courseType
      * @param courseType is used to assign the value of the instance variable courseType
-     * @throws InvalidValueException an error message is displayed in case the value entered for name does not meet the conditions set be this method
+     * @throws InvalidValueException an error message is displayed in case the value entered does not meet the conditions set in this method
      */
-    public void setCourseType(CourseType courseType) {
-        if(courseType != null) {       // sets a condition that the name may not be empty(null) or 1 character or less
-            this.se = name;
+    public void setCourseType(CourseType courseType) throws InvalidValueException{
+        if(courseType != null) {                        // checks that the courde type is not empty(null)
+            this.courseType = courseType;
         }else{                                          // the following error message is displayed
-            throw new InvalidValueException("Kursname muss mindestens 2 Zeichen lang sein!");
+            throw new InvalidValueException("Kurstyp darf nicht null/leer sein!");
         }
     }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", hours=" + hours +
+                ", beginDate=" + beginDate +
+                ", endDate=" + endDate +
+                ", courseType=" + courseType +
+                '}';
+    }
+
 }
+
