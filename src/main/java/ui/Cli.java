f0deba15d8c40ee1688830e4c44ad1cc12f2b1f5
Sonja Lechner <blaquekaktus@gmail.com>
2022-11-23 10:27:34 +0100

    Methode ausprogrammiert
    addCourse() (incomplete!)

--- src/main/java/ui/Cli.java
+++ src/main/java/ui/Cli.java
@@ -3,11 +3,14 @@ package ui;
 import dataaccess.MyCourseRepository;
 import dataaccess.MySqlDatabaseException;
 import domain.Course;
+import domain.CourseType;
+import domain.InvalidValueException;
 
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
+import java.sql.Date;
 
 /**
  * The Cli class is used to present a Command Line Interface menu to the user.
@@ -58,7 +61,7 @@ public class Cli {
             input = scan.nextLine();
 
             switch (input) {
-                case "1" -> System.out.println("Kurs Eingeben");
+                case "1" -> addCourse();
                 case "2" -> showAllCourses();
                 case "3" -> showCourseDetails();
                 case "x" -> System.out.println("Auf Wiedersehen");
@@ -66,6 +69,50 @@ public class Cli {
             }
         }
         scan.close();                   //closes the scanner
+    }
+
+    private void addCourse() throws MySqlDatabaseException, IllegalArgumentException{
+        String name, description;                               //Temporary Variables for each Object Property, used to store the information entered by the user
+        int hours;
+        Date beginDate, endDate;
+        CourseType courseType;
+
+
+        try{
+            System.out.println("Bitte alle Kursdaten angeben:");
+            System.out.println("Name: ");
+            name = scan.nextLine();
+            if(name.equals("")) throw new IllegalArgumentException("Eingabe darf nicht leer sein!"); //UI (Client-Side) Validation in order to ensure that the data entered is database consistent.
+            System.out.println("Description: ");
+            description = scan.nextLine();
+            if(description.equals("")) throw new IllegalArgumentException("Eingabe darf nicht leer sein!");
+            System.out.println("Hours: ");
+            hours = Integer.parseInt(scan.nextLine());
+            // Text Input (String) der CLI  converted to type 'int' through Integer.parseInt(number) method
+            // Through consistent use of consistent of 'scan.nextLine' instead of 'scan.nextInt' etc the risks of exceptions occurring due to the user pressing the enter key twice  is eliminated
+            if(hours<=0) throw new IllegalArgumentException("Eingabe muss mehr als 0 sein!");
+            System.out.println("Startdatum (YYYY-MM-DD): ");
+
+            System.out.println("Startdatum (YYYY-MM-DD): ");
+
+
+        }
+        catch(IllegalArgumentException illegalArgumentException){
+            // Handles exceptions thrown when the user enters one or more incorrect or illegal arguments. Returns an error message with the details of the exception.
+            System.out.println("Eingabefehler: " + illegalArgumentException.getMessage());
+        }
+        catch(InvalidValueException invalidValueException){
+            System.out.println("Kursdaten nicht korrekt eingegeben: " + invalidValueException.getMessage());
+            // Handles exceptions thrown when the user enters one or more invalid value. Returns an error message with the details of the exception.
+        }
+        catch(MySqlDatabaseException mySqlDatabaseException){
+            System.out.println("Datenbankfehler beim Einfügen: " + mySqlDatabaseException.getMessage());
+            // Handles exceptions thrown when the user SQL Database exceptions occurs. Returns an error message with the details of the exception.
+        }
+        catch(Exception exception){
+            System.out.println("Unbekannter Fehler beim Einfügen: " + exception.getMessage());
+            // Handles any other exceptions thrown. Returns an error message with the details of the exception.
+        }
     }
 
     /**
@@ -91,7 +138,11 @@ public class Cli {
         }
     }
 
-    private void showCourseDetails(){
+    /**
+     * Prints the details of the course corresponding to the ID entered by the user on the CLI
+     * If the course isn't found in the database an error message is shown
+     */
+    private void showCourseDetails() throws MySqlDatabaseException{
         System.out.println("Für welchen Kurs möchten Sie die kursdetails anzeigen?");
         Long courseId = Long.parseLong(scan.nextLine());
         try{
