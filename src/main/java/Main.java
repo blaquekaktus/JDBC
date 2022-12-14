7557524d43d2be2b3f3a17df65afa0b5cde69bf1
Sonja Lechner <blaquekaktus@gmail.com>
2022-12-14 02:43:12 +0100

    Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import dataaccess.MySQLDatabaseConnection;
 import dataaccess.MySqlCourseRepository;
+import dataaccess.MySqlStudentRepository;
 import ui.Cli;
 
 import java.sql.Connection;
@@ -9,10 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            Connection myConnection =
-                    MySQLDatabaseConnection.getConn("jdbc:mysql://localhost:3306/kurssystem", "root", "");
-
+        try{
+            MySQLDatabaseConnection.getConn("jdbc:mysql://localhost:3306/kurssystem", "root", "");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
@@ -28,6 +27,16 @@ public class Main {
             System.out.println("Datenbank Fehler: " + e.getMessage());
         }
         myCli.start();
+
+        Cli myCli2 = null;
+        try {
+            myCli2 = new Cli(new MySqlStudentRepository());
+        } catch (SQLException e) {
+            System.out.println("Datenbank Fehler: " + e.getMessage() + "\n SQL State: " + e.getSQLState());
+        } catch (ClassNotFoundException e) {
+            System.out.println("Datenbank Fehler: " + e.getMessage());
+        }
+        myCli2.start();
 
     }
 
